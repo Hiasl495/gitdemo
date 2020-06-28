c0cb0f76a1fe07550e1710ac48f0489639220e63
AndreasKrammer <a-krammer@online.de>
2020-06-28 10:57:13 +0200

    overhaul

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
 
-        System.out.println("new");
+        System.out.println();
     }
-
-    //Änderung1
-
-    //Änderung2
-
-    //Änderung3
 }
